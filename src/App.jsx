5c0cdd917bdac3aed2e99a8416c022555d82b71f
nelvoyant <nelvoyant@gmail.com>
2025-08-09 11:36:11 -0400

    log board context to console

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from "react";
-import BoardView from "./views/BoardView";
+import React, { useEffect } from "react";
+import mondaySdk from "monday-sdk-js";
 
-function App() {
-  return <BoardView />;
-}
+const monday = mondaySdk();
 
-export default App;
+const BoardView = () => {
+  useEffect(() => {
+    monday.listen("context", (res) => {
+      console.log("Board View Context:", res.data);
+    });
+  }, []);
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>👋 Hello from WorkClock Board View</h2>
+      <p>This is your board context area.</p>
+    </div>
+  );
+};
+
+export default BoardView;
